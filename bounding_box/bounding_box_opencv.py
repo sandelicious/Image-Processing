d88c3a8484d82e9d4e0746f058a99b372d86714e
Sandeep Kumar <sandeepkumar.tucsb12@gmail.com>
2019-10-07 16:38:35 +0530

    Update bounding_box_opencv.py

--- bounding_box/bounding_box_opencv.py
+++ bounding_box/bounding_box_opencv.py
@@ -1,6 +1,6 @@
 import cv2
 
-image = cv2.imread("C:/Users/I334125/OneDrive - SAP SE/Desktop/New Machine Learning Challenge/test1.png")
+image = cv2.imread("{Image Path}")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (9,9), 0)
 thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,12)
@@ -11,14 +11,14 @@ dilate = cv2.dilate(thresh, kernel, iterations=1)
 cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-ROI_number = 0
+box_number = 0
 for c in cnts:
     area = cv2.contourArea(c)
     x,y,w,h = cv2.boundingRect(c)
     cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
-    ROI = image[y:y+h, x:x+w]
-    cv2.imwrite('ROI_{}.png'.format(ROI_number), ROI)
-    ROI_number += 1
+    box = image[y:y+h, x:x+w]
+    cv2.imwrite('box_{}.png'.format(box_number), box)
+    box_number += 1
 cv2.imshow('dilate', dilate)
 cv2.imshow('image', image)
 cv2.waitKey()
